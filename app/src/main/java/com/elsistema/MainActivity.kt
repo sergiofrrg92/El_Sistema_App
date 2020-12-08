cc2e0691b85ecf048e8eb151314ae4edc6446abc
Sergiofrrg92 <sergiofrrg@gmail.com>
2020-12-08 21:45:47 +0000

    Half way through

--- app/src/main/java/com/elsistema/MainActivity.kt
+++ app/src/main/java/com/elsistema/MainActivity.kt
@@ -2,10 +2,43 @@ package com.elsistema
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.constraintlayout.motion.widget.TransitionAdapter
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val ml : MotionLayout = findViewById(R.id.motion_layout)
+
+        val viewModel = ViewModelProviders
+                .of(this)
+                .get(SwipeRightViewModel::class.java)
+
+        viewModel
+                .modelStream
+                .observe(this, Observer {
+                    bindCard(it)
+                })
+
+        ml.setTransitionListener(object: TransitionAdapter(){
+            override fun onTransitionCompleted(motionLayout: MotionLayout, currentId: Int) {
+                when (currentId) {
+                    R.id.offScreenPass,
+                    R.id.offScreenLike -> {
+                        motionLayout.progress = 0f
+                        motionLayout.setTransition(R.id.rest, R.id.like)
+                    }
+                }
+            }
+        })
+
+    }
+
+    private fun bindCard(it: Any) {
+
     }
 }
---
+++ app/src/main/java/com/elsistema/SwipeRightViewModel.kt
@@ -0,0 +1,43 @@
+package com.elsistema
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+
+class SwipeRightViewModel : ViewModel(){
+
+    private val stream = MutableLiveData<SwipeRightModel>()
+
+    val modelStream: LiveData<SwipeRightModel>
+        get() = stream
+
+    private val data = listOf(
+        SwipeRightCardModel(backgroundColor = Color.parseColor("#E91E63")),
+        SwipeRightCardModel(backgroundColor = Color.parseColor("#2196F3")),
+        SwipeRightCardModel(backgroundColor = Color.parseColor("#F44336")),
+        SwipeRightCardModel(backgroundColor = Color.parseColor("#9E9E9E"))
+    )
+    private var currentIndex = 0
+
+    private val topCard
+        get() = data[currentIndex % data.size]
+    private val bottomCard
+        get() = data[(currentIndex + 1) % data.size]
+
+    init {
+        updateStream()
+    }
+
+    fun swipe() {
+        currentIndex += 1
+        updateStream()
+    }
+
+    private fun updateStream() {
+        stream.value = SwipeRightModel(
+            top = topCard,
+            bottom = bottomCard
+        )
+    }
+
+
+}
